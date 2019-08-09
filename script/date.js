5cec2a54f1730309607b666c43b5f4d02f711ed5
lilkeys <53944335+lilkeys@users.noreply.github.com>
2019-08-09 20:38:04 +0600

    Update date.js

--- script/date.js
+++ script/date.js
@@ -1,1 +1,6 @@
-
+var m_names = new Array("01","02","03","04","05","06","07","08","09","10","11","12");
+var d = new Date();
+var twoDaysAgo = d.getDate()-3;
+var curr_month = d.getMonth();
+var curr_year = d.getFullYear();
+var x = twoDaysAgo + "." + m_names[curr_month] + "." + curr_year;
